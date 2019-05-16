fd41fc1c84e57344dd1fb28d606a63e9ee06f0ac
EC2 Default User <ec2-user@ip-172-31-32-40.ec2.internal>
2019-05-16 10:20:24 +0000

    Edit and View Profile Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -18,6 +21,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    flash[:success] = "Profile updated"
+    redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
 
     def user_params
@@ -28,6 +45,7 @@ class UsersController < ApplicationController
 
   def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
